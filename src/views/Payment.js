0141af6349dd2cd3f6a3f09d12d5927f8db6b32c
matkot11 <matkot11@gmail.com>
2021-02-04 19:14:54 +0000

    Added amount to payment component

--- src/views/Payment.js
+++ src/views/Payment.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import SidebarTemplate from 'templates/SidebarTemplate';
 import Form from 'components/organisms/Form/Form';
 import Input from 'components/atoms/Input/Input';
 import Button from 'components/atoms/Button/Button';
 import { routes } from 'routes';
-
+import getUser from 'composables/getUser';
+import getCollection from 'composables/getCollection';
 
 const StyledInnerWrapper = styled.div`
   display: flex;
@@ -13,19 +14,28 @@ const StyledInnerWrapper = styled.div`
   justify-content: space-evenly;
 `;
 
-const Payment = () => (
-  <SidebarTemplate>
-    <Form title="Payment">
-      <Input placeholder="Name on card" />
-      <Input placeholder="Card number" />
-      <StyledInnerWrapper>
-        <Input small placeholder="MM / YY" />
-        <Input small placeholder="CVC" />
-      </StyledInnerWrapper>
-      <Button to={routes.done}>Pay</Button>
-    </Form>
-  </SidebarTemplate>
-);
+const Payment = () => {
+  const { user } = getUser();
+  const { loadData, total } = getCollection();
 
+  useEffect(async () => {
+    if (user) {
+      await loadData(user.uid);
+    }
+  }, [user]);
+
+  return (
+    <SidebarTemplate>
+      <Form title="Payment">
+        <Input placeholder="Name on card" />
+        <Input placeholder="Card number" />
+        <StyledInnerWrapper>
+          <Input small placeholder="MM / YY" />
+          <Input small placeholder="CVC" />
+        </StyledInnerWrapper>
+        <Button to={routes.done}>Pay {total}£</Button>
+      </Form>
+    </SidebarTemplate>
+  );
+};
 export default Payment;
-
