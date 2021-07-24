0c0abc336a1f7b26b87da436fcdc7e210e7f2288
matkot11 <matkot11@gmail.com>
2021-07-24 20:01:23 +0100

    Changed template with sidebar

--- src/templates/Sidebar/Sidebar-styles.js
+++ src/templates/Sidebar/Sidebar-styles.js
@@ -3,10 +3,18 @@ import styled from 'styled-components';
 export const StyledWrapper = styled.div`
   display: flex;
   flex-direction: column;
+
+  @media only screen and (min-width: ${({ theme }) => theme.size.m}) {
+    flex-direction: row;
+  }
 `;
 
 export const StyledInnerWrapper = styled.div`
+  margin-top: 5rem;
   justify-self: center;
   align-self: center;
-  margin-top: 5rem;
+
+  @media only screen and (min-width: ${({ theme }) => theme.size.m}) {
+    margin: 5rem auto 0 auto;
+  }
 `;
